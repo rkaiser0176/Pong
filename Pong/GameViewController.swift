2ad2f12c3d1085e78a7dd4e5739a16240657531d
Ryan Kaiser <rkaiser0176@jh-mba-jacks.local>
2019-04-15 15:02:56 -0500

    removed the comment

--- Pong/GameViewController.swift
+++ Pong/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //subscribe to KyleTheChild
+        
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
