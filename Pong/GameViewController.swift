c13357a96a3000ab87c2838dd47cc7fc5705862e
Ryan Kaiser <rkaiser0176@jh-mba158-12.d214.org>
2019-04-15 15:02:17 -0500

    #subscribe to KyleTheChild

--- Pong/GameViewController.swift
+++ Pong/GameViewController.swift
@@ -14,6 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //subscribe to KyleTheChild
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
