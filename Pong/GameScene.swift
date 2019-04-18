76bbd85c94238ade87a5119ad6f4f9413b908a9d
Ryan Kaiser <rkaiser0176@stu.d214.org>
2019-04-18 15:06:17 -0500

    added some cool things

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -9,10 +9,30 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    
+    var topPaddle = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.physicsBody = border
+        topPaddle = self.childNode(withName: "topPaddle") as! SKSpriteNode
+        
     }
+    func didBegin(_ contact: SKPhysicsContact) {
+        print(contact.bodyA)
+        print(contact.bodyB)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
+        let location = touch.location(in: self)
+        topPaddle.run(SKAction.moveTo(x: location.x, duration: 0.2))
+    }
+    
 }
